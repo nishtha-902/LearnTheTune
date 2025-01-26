1a4402e408e5579315be59c9ce2425df507f051f
Nishtha <132751527+nishtha-902@users.noreply.github.com>
2025-01-26 21:51:16 +0530

    Update wavy-background.tsx

--- src/components/ui/wavy-background.tsx
+++ src/components/ui/wavy-background.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { cn } from "@/lib/utils";
 import React, { useEffect, useRef, useState } from "react";
